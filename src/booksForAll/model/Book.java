c5c12f4d4ccee308d417ed37d478428b45a75607
faresfaresCS <faresfares.cs@gmail.com>
2018-03-13 16:38:26 +0200

    rebase

--- src/booksForAll/model/Book.java
+++ src/booksForAll/model/Book.java
@@ -1,11 +1,46 @@
 package booksForAll.model;
 
 public class Book {
-	private int idBook;
+	public int getIdBooks() {
+		return idBooks;
+	}
+
+	public String getName() {
+		return Name;
+	}
+	
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	
+	public Float getPrice() {
+		return price;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public Book(int idBooks, String name, String author, String photo, Float price, String description) {
+		super();
+		this.idBooks = idBooks;
+		Name = name;
+		Author = author;
+		this.photo = photo;
+		this.price = price;
+		this.description = description;
+	}
+	private int idBooks;
 	private String Name;
 	private String Author;
 	private String photo;
 	private Float price;
 	private String description;
-	
 }
+
