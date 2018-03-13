cd0124da042480ee04268add31dec7be1f4a41ee
Fares Fares <faresfares.cs@gmail.com>
2018-03-13 16:14:14 +0200

    Fares (#6)
    * added htmls
    
    * forced

---
+++ src/booksForAll/model/Book.java
@@ -0,0 +1,11 @@
+package booksForAll.model;
+
+public class Book {
+	private int idBook;
+	private String Name;
+	private String Author;
+	private String photo;
+	private Float price;
+	private String description;
+	
+}
