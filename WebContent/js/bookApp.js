1f4be7e90a9bdffea9a535167d23c77ce01cb03a
Or B <or1502@gmail.com>
2018-03-21 09:55:24 +0200

    Clinet side - Angular integration
    Angular baseline

---
+++ WebContent/js/bookApp.js
@@ -0,0 +1,8 @@
+var bookApp = angular.module('bookApp', []);
+bookApp.controller('myController', ['$scope', function($scope) {
+    $scope.hey = "hi";
+}]);
+
+bookApp.controller('controller2',[function(){
+
+}]);
